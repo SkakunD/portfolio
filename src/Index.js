53dfb6fc8730e1d16982b4b3d8b9a683ff0603f2
SkakunD <beenom314@gmail.com>
2020-05-11 23:58:21 +0300

    add new page

--- src/Index.js
+++ src/Index.js
@@ -1,34 +1,66 @@
 import React from "react";
 import ReactDom from "react-dom";
 import Header from "../components/Header.jsx";
-import About from '../components/About.jsx';
-import Block from '../components/Block.jsx';
-import MyWork from '../components/MyWork.jsx'
+import About from "../components/About.jsx";
+import Block from "../components/Block.jsx";
+import MyWork from "../components/MyWork.jsx";
 // import "style.css";
 
 class App extends React.Component {
-    render() {
+  render() {
+    const blockItems = [
+      {
+        title: "MOBILE APP DESIGN",
+        paragraph:
+          "Применить свои знания для написания мобильной игры при помощи JS и фреймворков, но пока это планы.",
+        itemClass: "flexbox__icon flexbox__icon-app",
+      },
+      {
+        title: "WEB DESIGN",
+        paragraph:
+          "Научиться создавать уникальные по дизайну и работе сайты. Живые сайты которые будут радовать глаза пользователей.",
+        itemClass: "flexbox__icon flexbox__icon-design",
+      },
+      {
+        title: "REACT",
+        paragraph:
+          "Стараться использовать React как можно чаще для приобретения опыта",
+        itemClass: "flexbox__icon flexbox__icon-web",
+      },
+    ];
 
-        const blockItems = [
-            {title: "MOBILE APP DESIGN", paragraph: "Применить свои знания для написания мобильной игры при помощи JS и фреймворков, но пока это планы.", itemClass: "flexbox__icon flexbox__icon-app"},
-            {title: "WEB DESIGN", paragraph: "Научиться создавать уникальные по дизайну и работе сайты. Живые сайты которые будут радовать глаза пользователей.", itemClass: "flexbox__icon flexbox__icon-design"},
-            {title: "REACT", paragraph: "Стараться использовать React как можно чаще для приобретения опыта", itemClass: "flexbox__icon flexbox__icon-web" }
-        ];
+    const workItems = [
+      {
+        workHref: "https://github.com/SkakunD/practicalJS",
+        workSrc: "./img/github.jpg",
+      },
+      {
+        workHref: "http://vkontakte.ru/write170011272",
+        workSrc: "./img/vk-com.jpg",
+      },
+      {
+        workHref: "https://beenom.000webhostapp.com/index.html",
+        workSrc: "./img/page_reg.png",
+      },
+      {
+        workHref: "https://skakund.github.io/CarEngine",
+        workSrc: "./img/car.png",
+      },
+      //   {
+      //     workHref: "https://skakund.github.io/TestjQuery/",
+      //     workClass: "../img/github.jpg",
+      //   },
+    ];
 
-        const workItems = [
-            {workHref: "https://github.com/SkakunD/practicalJS", workSrc: "./img/github.jpg"},
-            {workHref: "http://vkontakte.ru/write170011272", workSrc: "./img/vk-com.jpg"},
-            {workHref: "https://beenom.000webhostapp.com/index.html", workSrc: "./img/page_reg.png"},
-            // {workHref: "https://skakund.github.io/TestjQuery/", workClass: "../img/github.jpg"}
-        ]
-
-        return <>
-            <Header/>
-            <About/>
-            <Block items={blockItems}/>
-            <MyWork linkHref={workItems}/>
-        </>
-    }
+    return (
+      <>
+        <Header />
+        <About />
+        <Block items={blockItems} />
+        <MyWork linkHref={workItems} />
+      </>
+    );
+  }
 }
 
-ReactDom.render(<App/>, document.querySelector("#root"))
+ReactDom.render(<App />, document.querySelector("#root"));
